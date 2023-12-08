2f2800e54f615a3d48ba6941bd6732505e36a42e
Maciej Bąba <maciek2baba2@gmail.com>
2023-12-08 14:17:14 +0100

    somethign

--- 2023/day1/part2.cpp
+++ 2023/day1/part2.cpp
@@ -1,76 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 
 using namespace std;
+
+int convert_string_num_to_digit(string str_num)
+{
+  map<string, int> strings_map;
+  strings_map["zero"] = 0;
+  strings_map["one"] = 1;
+  strings_map["two"] = 2;
+  strings_map["three"] = 3;
+  strings_map["four"] = 4;
+  strings_map["five"] = 5;
+  strings_map["six"] = 6;
+  strings_map["seven"] = 7;
+  strings_map["eight"] = 8;
+  strings_map["nine"] = 9;
+
+  if (strings_map.find(str_num) == strings_map.end())
+  {
+    return -1;
+  }
+
+  return strings_map[str_num];
+}
+
+string convert_line_with_str_nums_to_line_with_digits(string line)
+{
+  int digit_lengths[3] = {3, 4, 5};
+  const int MAX_LENGTH = 5;
+  const int MIN_LENGTH = 3;
+  string line_copy = line.substr(0, line.length());
+  if (line.length() < MAX_LENGTH)
+  {
+    return line;
+  }
+  for (int i = 0; i < line.length() - MAX_LENGTH; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      string str_num = line.substr(i, digit_lengths[j]);
+      if (convert_string_num_to_digit(str_num) >= 0)
+      {
+        int result = convert_string_num_to_digit(str_num);
+        string replace_with = to_string(result);
+        line_copy.replace(i, digit_lengths[j], replace_with);
+        break;
+      }
+    }
+  }
+  return line_copy;
+}
 
 int main()
 {
   ifstream file("input.txt");
-  struct strings_map
-  {
-    string zero = "0";
-    string one = "1";
-    string two = "2";
-    string three = "3";
-    string four = "4";
-    string five = "5";
-    string six = "6";
-    string seven = "7";
-    string eight = "8";
-    string nine = "9";
-  };
-  string strings[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-  int lengths[10] = {3, 4, 5};
-  string digit_str = "";
-  string num_str = "";
-  int global_sum;
+  string line;
 
   if (file.is_open())
   {
-    string line;
     while (getline(file, line))
     {
-      digit_str = "";
-      num_str = "";
       cout << line << endl;
-      for (int i = 0; i < line.length() - 5; i++)
-      {
-        for (int j = 0; j < sizeof(lengths); j++)
-        {
-          string temp = line.substr(i, lengths[j]);
-          if (temp == strings[j])
-          {
-            line = line.replace(i, lengths[j], strings_map().temp);
-            i += lengths[j] - 1;
-          }
-        }
-      }
-      cout << line << endl;
-      for (int i = 0; i < 10; i++)
-      {
-        if (line.find(strings[i]) != string::npos)
-        {
-          line = line.replace(line.find(strings[i]), strings[i].length(), to_string(i));
-        }
-      }
-      cout << line << endl;
-      for (int i = 0; i < line.length(); i++)
-      {
-        if (isdigit(line[i]))
-        {
-          digit_str += line[i];
-        }
-      }
-      num_str = string() + digit_str[0] + digit_str[digit_str.length() - 1];
-      cout << num_str << endl;
-      global_sum += stoi(num_str);
+      cout << convert_line_with_str_nums_to_line_with_digits(line) << endl;
     }
     file.close();
-    cout << global_sum << endl;
   }
   else
   {
-    cout << "Unable to open file" << endl;
+    cout << "Unable to open file";
   }
 }
