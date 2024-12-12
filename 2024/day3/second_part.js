4dd2571ffcf476cf0f62728875bb24126bb44e22
Maciej Baba <maciek2baba2@gmail.com>
2024-12-12 23:34:09 +0100

    finish day3 second part

--- 2024/day3/second_part.js
+++ 2024/day3/second_part.js
@@ -1,9 +1,25 @@
 import { input } from './data.js'
 
-const regex = /(mul\(\d{0,3},\d{1,3}\))(do\(\))(\don't\(\))/g
+const regex = /(mul\(\d{0,3},\d{1,3}\))|(do\(\))|(don't\(\))/g
 
 const inputArray = input.match(regex)
 
 let res = 0
+let skip = false
 
-console.log(inputArray)
+inputArray.forEach(item => {
+  console.log(item)
+  if (item.includes('do()')) {
+    skip = false
+  } else if (item.includes("don't()")) {
+    skip = true
+  }
+
+  if (!skip && item.includes('mul')) {
+    const [a, b] = item.match(/\d+/g)
+    const sum = a * b
+    res += sum
+  }
+})
+
+console.log(res)
